81147c4c8c4952e8356bd53c21c27ad12ce4d337
Jordan Holliday <j-holliday@DESKTOP-AALQUJS.localdomain>
2026-08-09 01:01:46 +0200

    test: record VTD-006 architect mutation evidence

--- acceptance/src/acceptance/verification_support/modular_architecture_vtd006_handlers.clj
+++ acceptance/src/acceptance/verification_support/modular_architecture_vtd006_handlers.clj
@@ -325,3 +325,7 @@
     :handler (fn [world _ _]
                (assert! world (= 5 (count (get-in world [:vtd006/evidence :contract :packInventory])))
                         "VTD-006 checkpoint evidence is incomplete."))}])
+
+;; clj-mutate-manifest-begin
+;; {:version 1, :tested-at "2026-08-09T00:58:20.809228442+02:00", :module-hash "-2009014110", :forms [{:id "form/0/ns", :kind "ns", :line 1, :end-line 4, :hash "1339117946"} {:id "form/1/defonce", :kind "defonce", :line 6, :end-line 6, :hash "701185655"} {:id "def/module-paths", :kind "def", :line 8, :end-line 16, :hash "415945835"} {:id "defn-/production-evidence!", :kind "defn-", :line 18, :end-line 28, :hash "59277045"} {:id "defn-/prepared", :kind "defn-", :line 30, :end-line 31, :hash "-223598626"} {:id "defn-/values", :kind "defn-", :line 33, :end-line 35, :hash "-45555851"} {:id "defn-/assert!", :kind "defn-", :line 37, :end-line 39, :hash "-1884999679"} {:id "defn-/pack-facts", :kind "defn-", :line 41, :end-line 43, :hash "1935968671"} {:id "defn-/consumer-scope", :kind "defn-", :line 45, :end-line 48, :hash "-640295098"} {:id "defn-/helper-planning", :kind "defn-", :line 50, :end-line 52, :hash "3897991"} {:id "defn/handlers", :kind "defn", :line 54, :end-line 327, :hash "-1613618921"}]}
+;; clj-mutate-manifest-end
