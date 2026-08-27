bb8d05ae64a1d1484cd45c0509eccf79394cc5ae
Jordan Holliday <j-holliday@DESKTOP-AALQUJS.localdomain>
2026-08-27 18:51:16 +0200

    Refresh cross-pack mutation manifests

--- test/acceptance/live_target_permission_path_apply_test.clj
+++ test/acceptance/live_target_permission_path_apply_test.clj
@@ -19,5 +19,5 @@
         "the path-apply acceptance scenarios pass as one focused contract")))
 
 ;; clj-mutate-manifest-begin
-;; {:version 1, :tested-at "2026-08-26T09:58:17.814735547+02:00", :module-hash "1661441014", :forms [{:id "form/0/ns", :kind "ns", :line 1, :end-line 5, :hash "92638594"} {:id "def/feature-path", :kind "def", :line 7, :end-line 7, :hash "-57033470"} {:id "form/2/deftest", :kind "deftest", :line 9, :end-line 19, :hash "-1497622675"}]}
+;; {:version 1, :tested-at "2026-08-27T17:52:43.549676857+02:00", :module-hash "1661441014", :forms [{:id "form/0/ns", :kind "ns", :line 1, :end-line 5, :hash "92638594"} {:id "def/feature-path", :kind "def", :line 7, :end-line 7, :hash "-57033470"} {:id "form/2/deftest", :kind "deftest", :line 9, :end-line 19, :hash "-1497622675"}]}
 ;; clj-mutate-manifest-end
