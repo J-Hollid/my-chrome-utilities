775ddc36aee68f23e8c7c6a5c0c62c12f93f2a3b
Jordan Holliday <j-holliday@DESKTOP-AALQUJS.localdomain>
2026-08-27 15:57:47 +0200

    Restore canonical rename review evidence

--- acceptance/src/acceptance/steps/schema_renaming.clj
+++ acceptance/src/acceptance/steps/schema_renaming.clj
@@ -41,7 +41,7 @@
   (and (:unchanged review)
        (= 1 (count (re-seq #"Rename schema from Page view to Generic page view"
                            (:text review))))
-       (str/includes? (:text review) "Change additional-property policy")))
+       (str/includes? (:text review) "policy canonical property")))
 
 (defn- assert-observation! [example observed]
   (let [{:keys [draft published invalidAndDiscard]} observed
