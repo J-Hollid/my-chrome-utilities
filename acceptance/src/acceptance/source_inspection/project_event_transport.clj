c5849d2c63ead7e608d1d5ab9684f68cd4110ee4
Jordan Holliday <j-holliday@DESKTOP-AALQUJS.localdomain>
2026-08-30 20:02:40 +0200

    Recognize installed transport persistence

--- acceptance/src/acceptance/source_inspection/project_event_transport.clj
+++ acceptance/src/acceptance/source_inspection/project_event_transport.clj
@@ -20,6 +20,9 @@
       (and (str/includes? source "configureProjectEventTransport")
            (str/includes? source "saveProjectEventTransport()")
            (boolean (re-find #"observationHistoryPath\s*:\s*historyPathInput(?:\?\.|\.)value" source))
+           (boolean (re-find #"historyPathInput(?:\?\.|\.)addEventListener\([\"']change[\"']" source)))
+      (and (str/includes? source "await ports.savePaths(snapshot)")
+           (boolean (re-find #"observationPath\s*:\s*historyPathInput(?:\?\.|\.)value" source))
            (boolean (re-find #"historyPathInput(?:\?\.|\.)addEventListener\([\"']change[\"']" source)))))
 
 (defn- history-path-input-listener? [source]
@@ -52,5 +55,5 @@
                        source))))))
 
 ;; clj-mutate-manifest-begin
-;; {:version 1, :tested-at "2026-08-30T19:59:01.029949954+02:00", :module-hash "-865414528", :forms [{:id "form/0/ns", :kind "ns", :line 1, :end-line 3, :hash "516389520"} {:id "def/owner", :kind "def", :line 5, :end-line 5, :hash "-1539356535"} {:id "def/history-path-input-source-snippets", :kind "def", :line 7, :end-line 8, :hash "-321170856"} {:id "defn/settings-source", :kind "defn", :line 10, :end-line 12, :hash "861841705"} {:id "defn-/history-path-input-present?", :kind "defn-", :line 14, :end-line 15, :hash "65075959"} {:id "defn-/history-path-input-persists?", :kind "defn-", :line 17, :end-line 23, :hash "1373493928"} {:id "defn-/history-path-input-listener?", :kind "defn-", :line 25, :end-line 26, :hash "866695440"} {:id "defn-/history-path-renderer?", :kind "defn-", :line 28, :end-line 30, :hash "1896447865"} {:id "defn/history-path-text-entry-wired?", :kind "defn", :line 32, :end-line 39, :hash "1962127616"} {:id "defn/history-path-incremental-entry-wired?", :kind "defn", :line 41, :end-line 52, :hash "2134396755"}]}
+;; {:version 1, :tested-at "2026-08-30T20:02:31.178355416+02:00", :module-hash "1516781517", :forms [{:id "form/0/ns", :kind "ns", :line 1, :end-line 3, :hash "516389520"} {:id "def/owner", :kind "def", :line 5, :end-line 5, :hash "-1539356535"} {:id "def/history-path-input-source-snippets", :kind "def", :line 7, :end-line 8, :hash "-321170856"} {:id "defn/settings-source", :kind "defn", :line 10, :end-line 12, :hash "861841705"} {:id "defn-/history-path-input-present?", :kind "defn-", :line 14, :end-line 15, :hash "65075959"} {:id "defn-/history-path-input-persists?", :kind "defn-", :line 17, :end-line 26, :hash "-1186009939"} {:id "defn-/history-path-input-listener?", :kind "defn-", :line 28, :end-line 29, :hash "866695440"} {:id "defn-/history-path-renderer?", :kind "defn-", :line 31, :end-line 33, :hash "1896447865"} {:id "defn/history-path-text-entry-wired?", :kind "defn", :line 35, :end-line 42, :hash "1962127616"} {:id "defn/history-path-incremental-entry-wired?", :kind "defn", :line 44, :end-line 55, :hash "2134396755"}]}
 ;; clj-mutate-manifest-end
